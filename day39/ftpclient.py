e7e3c50ffdb5f04a42e933b925c80aa38a8b36a7
longyunfeigu <2514553187@qq.com>
2017-11-21 19:44:31 +0800

    ftpserver v3

---
+++ day39/ftpclient.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+import struct
+import json
+import os
+import socketserver
+
+class FTPClient:
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    max_package_size = 8192
+    coding = 'utf-8'
+    client_dir = 'C:\\Users\\Administrator\\Desktop'
+
+    def __init__(self, server_address, connect=True):
+        """Constructor.  May be extended, do not override."""
+        self.server_address = server_address
+        self.connect = connect
+        self.socket = socket.socket(self.address_family, self.socket_type)
+
+        if connect:
+            try:
+                self.client_connect()
+            except:
+                self.client_close()
+                raise
+
+    def client_connect(self):
+        """Called by constructor to bind the socket.
+
+           May be overridden.
+        """
+        self.socket.connect(self.server_address)
+
+    def client_close(self):
+        """Called to clean-up the server.
+        May be overridden.
+        """
+        self.socket.close()
+
+    def run(self):
+        """merely processing client command"""
+        if not self.connect:
+            self.client_connect()
+        while True:
+            cmd = input('>>>:').strip()
+            if not cmd:
+                continue
+            cmd_list = cmd.split(' ')
+            command = cmd_list[0]
+            if hasattr(self, command):
+                func = getattr(self, command)
+                func(cmd_list)
+            else:
+                print('命令格式输入有误')
+
+    def put(self, args):
+        file_path = os.path.normpath(args[-1])
+        if not os.path.exists(file_path):
+            print('要上传的文件不存在')
+            return
+        else:
+            file_size = os.stat(file_path).st_size
+        # 发送头信息
+        headers_dict = {'command': args[0], 'filename': os.path.basename(file_path), 'filesize': file_size}
+        headers_json = json.dumps(headers_dict)
+        headers_bytes = headers_json.encode(self.coding)
+        self.socket.send(struct.pack('i', len(headers_bytes)))
+        self.socket.send(headers_bytes)
+
+        send_size = 0
+        with open(file_path, 'rb') as f:
+            for line in f:
+                already_send_size = len(line)
+                self.socket.send(line)
+                send_size += already_send_size
+                print(send_size)
+            else:
+                print('upload success')
+
+    def get(self, args):
+        # 发送头信息
+        headers_dict = {'command': args[0], 'filename': args[-1]}
+        headers_json = json.dumps(headers_dict)
+        headers_bytes = headers_json.encode(self.coding)
+        headers_length = len(headers_bytes)
+        self.socket.send(struct.pack('i', headers_length))
+        self.socket.send(headers_bytes)
+
+        headers_length = struct.unpack('i', self.socket.recv(4))[0]
+        print(headers_length)
+        headers_str = self.socket.recv(headers_length)
+        headers_dict = json.loads(headers_str)
+        err_msg = headers_dict.get('err_msg', None)
+        if err_msg:
+            print(err_msg)
+            return
+        file_path = os.path.join(self.client_dir, args[-1])
+        filesize = headers_dict.get('filesize')
+        already_recv_size = 0
+        with open(file_path, 'wb') as f:
+            while already_recv_size < filesize:
+                recv_data = self.socket.recv(self.max_package_size)
+                f.write(recv_data)
+                already_recv_size += len(recv_data)
+
+
+if __name__ == '__main__':
+    obj = FTPClient(('10.20.1.50', 13140))
+    obj.run()
+
+
+
--- day39/ftpserver.py
+++ day39/ftpserver.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import socket
 import struct
 import json
 import os
-import socketserver
+# import socketserver
 
 class FTPServer:
     address_family = socket.AF_INET
@@ -74,5 +77,67 @@ class FTPServer:
         while True:   # 链接循环
             self.conn, self.client_addr = self.get_request()
             print('from client...', self.client_addr)
-            pass
+            while True:
+                try:
+                    headers_struct = self.conn.recv(4)
+                    if not headers_struct:
+                        break
+                    # server收取头信息
+                    headers_length = struct.unpack('i', headers_struct)[0]
+                    print('***********', headers_length)
+                    headers_json = self.conn.recv(headers_length)
+                    print('-------', headers_json)
+                    headers_dict = json.loads(headers_json)
+                    print('***********', headers_dict)
 
+                    command = headers_dict.get('command')
+                    if hasattr(self, command):
+                        func = getattr(self, command)
+                        func(headers_dict)
+                except Exception as e:
+                    break
+
+    def put(self, args):
+        filename = args.get('filename')
+        filesize = args.get('filesize')
+        file_path = os.path.join(self.server_dir, filename)
+        already_recv_size = 0
+        with open(file_path, 'wb') as f:
+            while already_recv_size < filesize:
+                recv_data = self.conn.recv(self.max_package_size)
+                f.write(recv_data)
+                already_recv_size += len(recv_data)
+            else:
+                print('download success')
+    def get(self, args):
+        filename = args.get('filename')
+        filepath = os.path.join(self.server_dir, filename)
+        print(filepath)
+        if not os.path.exists(filepath):
+            err_msg = '服务器没有该文件'
+            headers_dict = {'err_msg': err_msg}
+        else:
+            filesize = os.path.getsize(filepath)
+            headers_dict = {'command': args[0], 'filename': filename, 'filesize': filesize}
+        # 发送数据之前先发数据的头信息
+        headers_str = json.dumps(headers_dict)
+        headers_bytes = headers_str.encode(self.coding)
+        headers_length = len(headers_bytes)
+        self.socket.send(struct.pack('i', headers_length))
+        self.socket.send(headers_bytes)
+        print('pppppppp')
+        send_size = 0
+        with open(filepath, 'rb') as f:
+            print('write.....')
+            for line in f:
+                already_send_size = len(line)
+                self.conn.send(line)
+                send_size += already_send_size
+                print(send_size)
+
+
+
+
+if __name__ == '__main__':
+    ftp_server = FTPServer(('10.20.1.50', 13140))
+    ftp_server.run()
